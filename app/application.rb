64bf0321242f22013ca737f79b0d26297e2a7ffc
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-10-15 14:31:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,13 @@
 class Application
 
   @@items = []
-  
+
   def call(env)
     req = Rack::Request.new(env)
     resp = Rack::Response.new
 
     if req.path.match(/items/)
- 
+
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       if item
@@ -15,10 +15,11 @@ class Application
       else
         resp.status = 400
         resp.write "We don't have this item"
+      end
     else
       resp.status = 404
     end
- 
+
     resp.finish
   end
 end
