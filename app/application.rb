e3c788ef8ed33f72ed13bedb5ad652a111896983
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-10-15 14:30:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,24 @@
 class Application
+
+  @@items = []
+  
   def call(env)
     req = Rack::Request.new(env)
     resp = Rack::Response.new
 
-    
+    if req.path.match(/items/)
+ 
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
+      if item
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "We don't have this item"
+    else
+      resp.status = 404
+    end
+ 
+    resp.finish
   end
 end
